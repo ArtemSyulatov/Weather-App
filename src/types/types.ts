cc9c90e252c1ea876f9d5c17d7f0e9fde204d4b3
ArtemSyulatov <artem.syulatov.11111@gmail.com>
2024-01-03 13:35:27 +0500

    weather card types add

---
+++ src/types/types.ts
@@ -0,0 +1,8 @@
+export interface IMain {
+    temp: number
+    humidity: number
+}
+
+export interface IWind {
+    speed: number
+}
--- src/utils/getIcon.ts
+++ src/utils/getIcon.ts
@@ -1,4 +1,3 @@
 export const getIcon = (data: string) => {
-    console.log('done')
     return `https://openweathermap.org/img/wn/${data}@2x.png`
 }
